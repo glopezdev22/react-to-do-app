b2ac8af778f51da9fc252ad1163a584d49adf90d
glopezdev22 <glopezdev22@gmail.com>
2026-05-13 23:36:33 -0600

    refactor: initialize tasks state as empty array

--- src/features/todos/hooks/useTasks.js
+++ src/features/todos/hooks/useTasks.js
@@ -3,14 +3,7 @@ import { useState } from "react";
 import React from 'react'
 
 export const useTasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            title: "Ordenar la casa",
-            content: "Debo ordenar la casa antes de las 12 pm",
-            date: "12/05/26"
-        }
-    ]);
+    const [tasks, setTasks] = useState([]);
     return (
         {tasks}
     )
